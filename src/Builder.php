617b42fac49d24d005ff20d595e53f04a0d65571
dmz9 <dmz9@yandex.ru>
2017-06-12 22:58:12 +0300

    populate builder

---
+++ src/Builder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace InsalesApi;
+class Builder
+{
+	public static function populate(&$object, array $data)
+	{
+		foreach ($data as $key => $value) {
+			$method = 'set' . ucfirst($key);
+			if (method_exists(
+				$object,
+				$method
+			)) {
+				$object->$method($value);
+			} else if (property_exists(
+				$object,
+				$key
+			)) {
+				$object->{$key} = $value;
+			} else if (isset($object->unknownProperties) && is_array($object->unknownProperties)) {
+				$object->unknownProperties[$key] = $value;
+			}
+		}
+	}
+}
