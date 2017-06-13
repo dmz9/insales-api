01ec7ff2c57100aa6e3f1e53919d4a577d6a734d
dmz9 <dmz9@yandex.ru>
2017-06-14 00:47:01 +0300

    payment gateway lodash

--- src/Api/PaymentGateway/PaymentGateway.php
+++ src/Api/PaymentGateway/PaymentGateway.php
@@ -12,7 +12,7 @@ class PaymentGateway extends AbstractApi
 	{
 		$prepared = $this->prepare(
 			$request->asArray(),
-			'payment-gateway'
+			'payment_gateway'
 		);
 		
 		$rawResponse = $this->transport->post(
@@ -39,7 +39,7 @@ class PaymentGateway extends AbstractApi
 	{
 		$prepared = $this->prepare(
 			$request->asArray(),
-			'payment-gateway'
+			'payment_gateway'
 		);
 		
 		$rawResponse = $this->transport->post(
