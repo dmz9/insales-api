7a3f04a3667f4569f983fbca41ed23a0687867d1
dmz9 <dmz9@yandex.ru>
2017-06-15 18:54:52 +0400

    more proper xml disassembler

--- src/Helper.php
+++ src/Helper.php
@@ -15,28 +15,39 @@ class Helper
 				? $node = array($node)
 				: 1) && $node = &$node[];
 
-			$node = $element->count()
-				? self::XML2Array($element)
-				: trim($element);
+			if($element->count()){
+				$node=self::XML2Array($element);
+			}else if(""!=$element){
+				$node=trim($element);
+			}else{
+				$node=($element['type']=='array')?array():null;
+			}
 		}
 
 		return $array;
 	}
-	
+
 	public static function array2XML(\SimpleXMLElement $root, array $data)
 	{
 		foreach ($data as $key => $value) {
 			if (is_array($value)) {
 				$new_object = $root->addChild($key);
-				self::array2XML($new_object, $value);
+				self::array2XML(
+					$new_object,
+					$value
+				);
 			} else {
-				$root->addChild($key, $value);
+				$root->addChild(
+					$key,
+					$value
+				);
 			}
 		}
 	}
+
 	public static function isAssociativeArray($array)
 	{
-		if(!is_array($array)){
+		if (!is_array($array)) {
 			return false;
 		}
 		if (array() === $array) {
@@ -47,10 +58,13 @@ class Helper
 				count($array) - 1
 			);
 	}
-	
+
 	public static function pre($var)
 	{
-		echo "<pre>".print_r($var,1)."</pre>";
+		echo "<pre>" . print_r(
+				$var,
+				1
+			) . "</pre>";
 		echo "\n\r";
 	}
 }
